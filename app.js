b2a440b81ef4fcf239e6aa5bd64cd8e760ac8e72
mhmadalaa <mhmaaddalaa@gmail.com>
2023-09-27 12:42:45 +0300

    feat: handling post requests wiht express

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 require('dotenv').config();
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+// middleware
+// to accept the incoming json post request
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'Hello from Natrous app',
     user: 'malaa',
   });
+});
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.post('/', (req, res) => {
