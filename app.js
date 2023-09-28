ec595b81de49b669145c55269ee39a3e6bb6e34e
mhmadalaa <mhmaaddalaa@gmail.com>
2023-09-28 14:18:31 +0300

    feat: try different code configurations

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 require('dotenv').config();
 const fs = require('fs');
 const express = require('express');
-
 const app = express();
 
-// middleware
-// to accept the incoming json post request
 app.use(express.json());
 
 const tours = JSON.parse(
@@ -27,6 +24,22 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const tour = tours.find((el) => el.id === parseInt(req.params.id));
+
+  if (tour) {
+    res.status(200).json({
+      status: 'success',
+      tour: tour,
+    });
+  } else {
+    res.status(404).json({
+      status: 'fail',
+      message: 'tour not founded',
+    });
+  }
 });
 
 app.post('/api/v1/tours', (req, res) => {
@@ -51,6 +64,17 @@ app.post('/api/v1/tours', (req, res) => {
 
 app.post('/', (req, res) => {
   res.status(200).send('post method...');
+});
+
+app.patch('/api/v1/tours', (req, res) => {
+  res.status(200).send('pathch...');
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  res.status(202).json({
+    status: 'success',
+    data: null,
+  });
 });
 
 app.listen(process.env.PORT, process.env.HOST, () => {
