7d27bba8d5905a36daa7a95dc7f8c6a9481149c9
mhmadalaa <mhmaaddalaa@gmail.com>
2023-11-09 04:10:23 +0200

    feat: rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -13,12 +14,19 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again later!',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static('./public')); // to server static file not from the route
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
   next();
 });
 
