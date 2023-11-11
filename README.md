066678bf0312ac4d1c258b682302b4b337d29503
mhmadalaa <mhmaaddalaa@gmail.com>
2023-11-11 22:27:43 +0200

    docs: add REDME.md

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1 align=center> Tour booking REST API</h1>
+
+### Tools and Technologies
+
+- [Node.js](https://nodejs.org/en/) - JS runtime environment.
+- [Express](http://expressjs.com/) - web framework.
+- [Mongoose](https://mongoosejs.com/) - Object Data Modelling library.
+- [MongoDB Atlas](https://www.mongodb.com/cloud/atlas) - Cloud database service.
+- [JSON Web Token](https://jwt.io/) - Security token.
+- [Postman](https://www.getpostman.com/) - API testing.
+
+```
+  🚩 The project is still Under Construction
+```
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,25 +4,59 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
-router.patch('/confirm-signup/:confirmToken', authController.confirmSignup);
-router.post('/login', authController.login);
+router.post(
+  '/signup', 
+  authController.signup
+);
 
-router.patch('/update-password', authController.protect, authController.updatePassword);
+router.patch(
+  '/confirm-signup/:confirmToken', 
+  authController.confirmSignup
+);
 
-router.post('/forgot-password', authController.forgetPassword);
-router.patch('/reset-password/:resetToken', authController.resetPassword); 
+router.post(
+  '/login', 
+  authController.login
+);
 
-router.post('/change-email', authController.protect, authController.changeEmail);
-router.patch('/reset-email/:resetToken', authController.protect, authController.resetEmail);
+router.patch(
+  '/update-password',
+  authController.protect,
+  authController.updatePassword,
+);
 
-router.patch('/update-user', authController.protect, userController.updateUser);
-router.delete('/delete-user', authController.protect, userController.deleteUser);
+router.post(
+  '/forgot-password',
+  authController.forgetPassword
+);
 
+router.patch(
+  '/reset-password/:resetToken', 
+  authController.resetPassword
+);
 
-// TODO: LOGOUT user
-// router.post('/logout', authController.login, authController.logout);
+router.post(
+  '/change-email',
+  authController.protect,
+  authController.changeEmail,
+);
+router.patch(
+  '/reset-email/:resetToken',
+  authController.protect,
+  authController.resetEmail,
+);
 
+router.patch(
+  '/update-user', 
+  authController.protect, 
+  userController.updateUser
+);
+
+router.delete(
+  '/delete-user',
+  authController.protect,
+  userController.deleteUser,
+);
 
 // FIXME: THESE ROUTERS JUST FOR DEBUGGING, NOT READY YET FOR PRODUCTION USEAGE
 router.route('/').get(userController.getAllUsers);
