595e3efd054637a7b69e3c16992974576c10b253
mhmadalaa <mhmaaddalaa@gmail.com>
2023-12-02 23:51:38 +0200

    fix: prevent populating sensitive data

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,20 +34,17 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
-// reviewSchema.pre(/^find/, function (next) {
-//   this.populate(
-//     {
-//       path: 'tour',
-//       select: '-__v',
-//     },
-//     {
-//       path: 'user',
-//       select: '-__v',
-//     },
-//   );
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
 
-//   next();
-// });
+  next();
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 
